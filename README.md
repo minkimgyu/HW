fdc94e637b3a3f7482ce2913c8a8de245643fb93
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2023-04-15 17:47:40 +0900

    update HW4

---
+++ README.md
@@ -0,0 +1,3 @@
+# HW
+
+Developed with Unreal Engine 5
---
+++ Source/HW/HWGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HWGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HW_API AHWGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
